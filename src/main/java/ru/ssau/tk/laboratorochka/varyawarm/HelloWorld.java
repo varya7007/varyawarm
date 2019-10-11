cee1767e11a622fbf48cb9ef4172709c683f713b
Sherukhaeva <sher_var@mail.ru>
2019-10-12 01:07:41 +0400

    upload 1.1, 1.2

--- src/main/java/ru/ssau/tk/laboratorochka/varyawarm/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package ru.ssau.tk.laboratorochka.varyawarm;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
---
+++ src/test/java/ru/ssau/tk/cherrypie/varyawarm/PersonTest.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.cherrypie.varyawarm;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PersonTest {
+@Test
+
+}
