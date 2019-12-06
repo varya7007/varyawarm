11ec00de8579321a7e04db65b7e4f770d8fa3b62
Sherukhaeva <sher_var@mail.ru>
2019-12-07 01:12:26 +0400

    upd pom. + 1.4

---
+++ src/main/java/ru/ssau/tk/cherrypie/varyawarm/Points.java
@@ -0,0 +1,26 @@
+package ru.ssau.tk.cherrypie.varyawarm;
+
+public class Points{
+    public static Point sum(Point point1, Point point2) {
+        Point sumPoint = new Point(point1.x + point2.x, point1.y + point2.y, point1.z + point2.z);
+        return sumPoint;
+    }
+
+    public static Point subtract(Point point1, Point point2) {
+        Point subtractPoint = new Point(point1.x - point2.x, point1.y - point2.y, point1.z - point2.z);
+        return subtractPoint;
+    }
+
+    public static Point multiply(Point point1, Point point2) {
+        Point multiplyPoint = new Point(point1.x * point2.x, point1.y * point2.y, point1.z * point2.z);
+        return multiplyPoint;
+    }
+
+    public static Point divide(Point point1, Point point2) {
+        Point dividePoint = new Point(point1.x / point2.x, point1.y / point2.y, point1.z / point2.z);
+        return dividePoint;
+    }
+
+    private Points() {
+    }
+}
