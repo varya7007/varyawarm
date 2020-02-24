56952fc3ef32dce48ab7e16cca75030a228569e2
Sherukhaeva <sher_var@mail.ru>
2020-02-24 16:00:51 +0400

    2.1

--- src/main/java/ru/ssau/tk/cherrypie/varyawarm/arrays/Array.java
+++ src/main/java/ru/ssau/tk/cherrypie/varyawarm/arrays/Array.java
@@ -5,5 +5,25 @@ public class Array {
     public static double[] array(int length) {
         return new double[length];
     }
+
+    public static int[] eArray(int length) {
+        int[] array = new int[length];
+        array[0] = 2;
+        for (int i = 1; i < length - 2; i++) {
+            array[i] = 1;
+        }
+        array[length - 1] = 2;
+        return array;
+    }
+
+    public static int[] oddArray(int length) {
+        int[] array = new int[length];
+        array[0] = 1;
+        for (int i = 1; i < length - 1; i++) {
+            array[i] = array[i - 1] + 2;
+        }
+        return array;
+    }
+
 }
 
--- src/test/java/ru/ssau/tk/cherrypie/varyawarm/arrays/ArrayTest.java
+++ src/test/java/ru/ssau/tk/cherrypie/varyawarm/arrays/ArrayTest.java
@@ -5,10 +5,32 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class ArrayTest {
-    private int legth = 5;
+    private int length = 5;
+
     @Test
     public void testArray() {
-        double[] array = Array.array(legth);
-        assertEquals(array.length,legth);
+        double[] array = Array.array(length);
+        assertEquals(array.length, length);
+    }
+
+
+    @Test
+    public void testEArray() {
+        int[] array = Array.eArray(length);
+        assertEquals(array[0], 2);
+        assertEquals(array[length - 1], 2);
+        for (int i = 1; i < length - 2; i++) {
+            assertEquals(array[i], 1);
+        }
+    }
+
+    @Test
+    public void testOddArray() {
+        int[] array = Array.oddArray(length);
+        assertEquals(array[0], 1);
+        for (int i = 1; i < length - 1; i++) {
+        assertEquals(array[i],array[i-1]+2);
+
+        }
     }
 }
