6cdee1a78ca869096e95dce6d434407d4712ba14
Sherukhaeva <sher_var@mail.ru>
2020-02-24 19:04:12 +0400

    3.2

--- src/main/java/ru/ssau/tk/cherrypie/varyawarm/string/PartString.java
+++ src/main/java/ru/ssau/tk/cherrypie/varyawarm/string/PartString.java
@@ -2,8 +2,15 @@ package ru.ssau.tk.cherrypie.varyawarm.string;
 
 public class PartString {
     static void Task1(String string) {
-        for (int i = 0; i < string.length() - 1; i++) {
+        for (int i = 0; i < string.length(); i++) {
             System.out.println(string.charAt(i));
+        }
+    }
+
+    static void Task2(String string) {
+        byte[] bytes = string.getBytes();
+        for (byte byt: bytes) {
+            System.out.println(byt);
         }
     }
 }
--- src/test/java/ru/ssau/tk/cherrypie/varyawarm/string/PartStringTest.java
+++ src/test/java/ru/ssau/tk/cherrypie/varyawarm/string/PartStringTest.java
@@ -5,10 +5,15 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class PartStringTest {
-    private String string = "символы символы";
+    private String string = "символы symbols";
 
     @Test
     public void testTask1() {
         PartString.Task1(string);
+    }
+
+    @Test
+    public void testTask2() {
+        PartString.Task2(string);
     }
 }
